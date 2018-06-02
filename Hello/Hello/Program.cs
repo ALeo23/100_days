4ed6bb73cb9e41350039d07495ca5600aac32a28
Alex Leo <aleo1337@gmail.com>
2018-06-02 13:13:37 -0400

    update gitignore and add hours of sleep to program

--- Hello/Hello/Program.cs
+++ Hello/Hello/Program.cs
@@ -6,7 +6,23 @@ namespace Hello
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello " + args[0]);
+            Console.WriteLine("What's your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("How many hours of sleep did you get last night?");
+            int hoursOfSleep = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Hello, " + name);
+
+            if (hoursOfSleep >= 8)
+            {
+                Console.WriteLine("You are well rested!");
+            }
+            else
+            {
+                Console.WriteLine("Get more sleep!");
+            }
         }
     }
 }
